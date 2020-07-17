0cd5dc3c81bf5d88df08409defeeee14b02b69f6
rnxpyke <rnxpyke@gmail.com>
2020-07-17 14:11:16 +0200

    add banner toggle

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-async function allowPush() {
+var bannerInteraction = false;
+
+
+
+function toggleBanner() {
+    bannerInteraction = true;
+    let banners = document.getElementsByClassName("notify-banner");
+    Array.from(banners).forEach(b => b.classList.toggle("hidden"));
+}
+
+async function requestPush() {
     console.log("allow");
     let perm = await Notification.requestPermission();
-    console.log(perm);
     if (perm === "granted") {
         var note = new Notification("Hello World!");
+    } else {
+        console.error("user denied notification permission");
     }
 }
 
 async function denyPush() {
-    console.log("deny");
+    bannerInteraction = true;
+    let banners = document.getElementsByClassName("notify-banner");
+    Array.from(banners).forEach(b => b.classList.add("hidden"));
 }
