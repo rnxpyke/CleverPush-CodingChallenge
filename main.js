f6099d0c458f20ed4b944ecfb31f409526cef1f5
rnxpyke <rnxpyke@gmail.com>
2020-07-17 14:59:52 +0200

    add final doumentation

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
 // Tracks if the User already has interacted with the popup.
 var popupInteraction = false;
 
-// Helper fun
+// Helper function to get all popups by class
 function popups() {
     return Array.from(document.getElementsByClassName("notify-popup"));
 }
 
+// Show Popup if User didn't interact and doesn't block/allow notifications
+// already
 setTimeout(() => {
     if (!popupInteraction && !(Notification.permission !== "default")) {
         popups().forEach(b => b.classList.remove("hidden"));
     }
 }, 3000);
 
+// Toggle the visibility of the popup
 function togglePopup() {
     popupInteraction = true;
     popups().forEach(b => b.classList.toggle("hidden"));
 }
 
+
+// allow action:
+// request notification permisson and show hello word notification
 async function requestPush() {
     let perm = await Notification.requestPermission();
     if (perm === "granted") {
@@ -26,7 +32,9 @@ async function requestPush() {
     }
 }
 
-async function denyPush() {
+// no, thinks action:
+// close the popup
+function denyPush() {
     popupInteraction = true;
     popups().forEach(b => b.classList.add("hidden"));
 }
